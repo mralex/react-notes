47891f7e009cacb46f560ec27214af4024888c9a
Alex Roberts <alex@nixiecraft.com>
2018-05-03 13:38:56 -0700

    Serviceworker/PWA note on intro...

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ class IndexPage extends React.PureComponent {
                     <h2>Welcome to ReactNotes</h2>
                     <p>You don't have any notes. <Link className="btn" to="/new">Get started!</Link></p>
                     <p>All notes you create will be stored right here, on your browser. No need to worry about logging in.</p>
+                    <p>This app also uses a ServiceWorker, so even if you're offline, you can still access and update your notes! And if your device supports it, you can save this page to your home screen and use it like any other app on your phone.</p>
                     <p>There's currently no Import/Export functionality, so you will not be able to see or share notes written using other browsers or on your phone.</p>
                     <hr/>
                     <p>This app is <a href="https://github.com/mralex/react-notes">open source</a>, built using React and PouchDB. Developed for a tutorial series on YouTube.</p>
